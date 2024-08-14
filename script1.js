31df7df0c712c0786538279e31ce2da9d2b3c8c9
larem <emmanuelosei831@gmail.com>
2024-08-14 02:25:00 +0000

    new work

---
+++ script1.js
@@ -0,0 +1,23 @@
+// Toggle mobile menu
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+console.log("hello");
+
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const viewMoreButton = document.querySelector('.view-more');
+    const hiddenItems = document.querySelectorAll('.portfolio-item.hidden');
+  
+    viewMoreButton.addEventListener('click', () => {
+      hiddenItems.forEach(item => {
+        item.classList.remove('hidden');
+      });
+      viewMoreButton.style.display = 'none'; // Hide the button after showing all items
+    });
+  });
+  
